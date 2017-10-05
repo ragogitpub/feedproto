797069c60513dbe8bcfbfc4329d6e7239448103d
rajesh <rajesh.goswami@gmail.com>
2017-10-05 14:59:16 +0530

    add logic to retrieve agency url/user/pwd settings from environment

--- feedProcessor/index.js
+++ feedProcessor/index.js
@@ -1,11 +1,25 @@
 const $SP = require('sharepointplus');
 
 module.exports = function (context, myQueueItem) {
-    context.log('JavaScript queue trigger function processed work item', myQueueItem);
+    context.log('Processing', myQueueItem);
 
     var agencyName = myQueueItem.nc4__agencyName;
     var listName = myQueueItem.nc4__listName;
     var idField = myQueueItem.nc4__idField;
+
+    context.log( 
+		'agency=', 
+		agencyName, 
+		'list=', 
+		listName, 
+		'idField=', 
+		idField,
+		'url=',
+		urlForAgency( agencyName ),
+		'username=',
+		userForAgency( agencyName ),
+		'password=',
+		passwordForAgency( agencyName ) );
 
     var itemJSON = myQueueItem;
     itemJSON.PartitionKey = agencyName + '-' + listName;
@@ -14,3 +28,23 @@ module.exports = function (context, myQueueItem) {
 
     context.done();
 };
+
+function settingForAgency(agencyName, settingName)
+{
+    return process.env[ agencyName + '.' + settingName ];
+}
+
+function urlForAgency( agencyName )
+{
+    return settingForAgency( agencyName, 'url' );
+}
+
+function userForAgency( agencyName )
+{
+    return settingForAgency( agencyName, 'username' )
+}
+
+function passwordForAgency( agencyName )
+{
+    return settingForAgency( agencyName, 'password' );
+}
