a7356b0d870e12ccb864501bf615241eadf17819
rajesh <rajesh.goswami@gmail.com>
2017-10-05 14:27:12 +0530

    get agency, list and idfield from JSON itself

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -1,9 +1,15 @@
+const $SP = require('sharepointplus');
+
 module.exports = function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
 
+    var agencyName = myQueueItem.agencyName;
+    var listName = myQueueItem.listName;
+    var idField = myQueueItem.idField;
+
     var itemJSON = myQueueItem;
-    itemJSON.PartitionKey = 'Test';
-    itemJSON.RowKey = itemJSON.CADItemNo + '-' + (new Date()).toISOString();
+    itemJSON.PartitionKey = agencyName + '-' + listName;
+    itemJSON.RowKey = myQueueItem[ idField ] + '-' + (new Date()).toISOString();
     context.bindings.tableContent = [ itemJSON ];
 
     context.done();
