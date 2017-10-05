bb8be7b6290d084d50133c9c829a822091569e1b
rajesh <rajesh.goswami@gmail.com>
2017-10-05 13:13:14 +0530

    remove extra samples

--- cs-ignoreme/run.csx
+++
@@ -1,6 +0,0 @@
-using System.Net;
-
-public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log)
-{
-    return req.CreateResponse(HttpStatusCode.OK, "Never pack me");
-}
--- scriptFile/usemeinstead.js
+++
@@ -1,9 +0,0 @@
-module.exports = module.exports = function (context, req) {
-    context.log('"scriptFile.usemeinstead" function called');
-    const res = {
-        body: {
-            "success":true
-        }
-    }
-    context.done(null, res);
-};
